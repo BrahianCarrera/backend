6a6082c5c1d1b10696000797bfedd17f374d8f9c
BrahianCarrera <brahian.carrera@udea.edu.co>
2025-06-09 23:49:15 -0500

    sonarcloud added

--- src/test/java/com/FleetGuard360/backend/BackendApplicationTests.java
+++ src/test/java/com/FleetGuard360/backend/BackendApplicationTests.java
@@ -1,13 +1,65 @@
 package com.FleetGuard360.backend;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.FleetGuard360.backend.repository.AlertRepository;
+import com.FleetGuard360.backend.repository.UserRepository;
+import com.FleetGuard360.backend.service.AlertService;
+import com.FleetGuard360.backend.service.UserService;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Mock
+    private UserRepository userRepository;
+
+	@Mock
+	private AlertRepository alertRepository;
+
+
+    @InjectMocks
+    private UserService userService;
+	
+	@InjectMocks
+	private AlertService alertService; 
+
+	private  BCryptPasswordEncoder passwordEncoder;
+
+	@BeforeEach
+	void setup(){
+
+		this.passwordEncoder = new BCryptPasswordEncoder();
+		userService = new UserService(passwordEncoder);
+		alertService = new AlertService();
+	}
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void createUser() {
+	}
+
+	@Test
+	void loginUser() {
+	}
+
+	@Test
+	void createAlert(){
+
+	}
+
+	@Test
+	void deleteAlert(){
+
+	}
+
+
 
 }
